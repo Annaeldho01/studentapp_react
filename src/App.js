9700a32307471e404a4f44986a6f744bd4453503
Annaeldho01 <annaeldho001@gmail.com>
2024-06-07 14:20:51 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import SearchStudent from './components/SearchStudent';
 import DeleteStudent from './components/DeleteStudent';
 import ViewStudent from './components/ViewStudent';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddStudent/>
-      <SearchStudent/>
-      <DeleteStudent/>
-      <ViewStudent/>
-      
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AddStudent/>}/>
+      <Route path="/search" element={<SearchStudent/>}/>
+      <Route path="/delete" element={<DeleteStudent/>}/>
+      <Route path="/view" element={<ViewStudent/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
