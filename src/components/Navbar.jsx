4845151e11855a278f189be7a5c36a65bef4b8f9
Annaeldho01 <annaeldho001@gmail.com>
2024-06-07 15:09:03 +0530

    project completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -6,23 +7,23 @@ const Navbar = () => {
         
         <nav className="navbar navbar-expand-lg bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">STUDENT APP</a>
+    <Link className="navbar-brand" to="#">STUDENT APP</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Add Student</a>
+          <Link className="nav-link active" aria-current="page" to="/">Add Student</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/search">Search Student</a>
+          <Link className="nav-link" to="/search">Search Student</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/delete">Delete Student</a>
+          <Link className="nav-link" to="/delete">Delete Student</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/view">ViewAll</a>
+          <Link className="nav-link" to="/view">ViewAll</Link>
         </li>
       </ul>
     </div>
