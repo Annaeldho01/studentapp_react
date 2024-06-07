ac85e235ae169bde4e9a3637b24c39b04669a555
Annaeldho01 <annaeldho001@gmail.com>
2024-06-07 13:39:20 +0530

    project completed

---
+++ src/components/ViewStudent.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewStudent = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1234</h5>
+                        <p className="card-text">Arpitha Shiju</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1235</h5>
+                        <p className="card-text">Ashna D</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1236</h5>
+                        <p className="card-text">Anusha K</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1237</h5>
+                        <p className="card-text">Bhagya K</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1238</h5>
+                        <p className="card-text">Anu Augustine</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1239</h5>
+                        <p className="card-text">Ardra C</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1241</h5>
+                        <p className="card-text">Arpith k</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div align="center"className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div className="card-body">
+                         <h5 className="card-title">Admission No:1257</h5>
+                        <p className="card-text">ASIF K</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1266</h5>
+                        <p className="card-text">Aashi M</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                        <div className="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrab-yFwpW_fFwZqPUMPpmLJ2fLs7eAxBEhg&s" class="card-img-top" alt="..."></img>
+                        <div align="center"className="card-body">
+                         <h5 className="card-title">Admission No:1334</h5>
+                        <p className="card-text">Mazha Dev</p>
+                        <a href="#" class="btn btn-primary">VIEW MORE</a>
+                          </div>
+                       </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewStudent
